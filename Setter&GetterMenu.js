2a275e96c5daabb6d0c8611da50a81a901eb8470
pipsies <filipabartlett@gmail.com>
2022-04-27 20:06:26 +0200

    Setter&GetterObjectsMenuProject

---
+++ Setter&GetterMenu.js
@@ -0,0 +1,36 @@
+const menu = {
+    _meal: '',
+    _price: 0,
+    
+    set meal(mealToCheck) {
+      if (typeof mealToCheck === 'string') {
+        return this._meal = mealToCheck;
+      }
+    },
+    
+    set price(priceToCheck) {
+      if (typeof priceToCheck == 'number') {
+        return this._price = priceToCheck;
+      }
+    },
+    
+    get todaysSpecial() {
+    
+    if (this._meal && this._price) {
+      return `Todays special is ${this._meal} for ${this._price}`; 
+    } else {
+      return `Meal or price was not set correctly!`;
+    }
+    }
+    };
+    
+    menu.meal = 'Chicken';
+    menu.price = 10;
+    console.log(menu.todaysSpecial);
+    
+    
+    
+    
+    
+    
+    
